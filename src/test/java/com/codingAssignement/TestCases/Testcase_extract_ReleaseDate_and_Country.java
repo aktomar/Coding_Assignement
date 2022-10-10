cef474fdf88508c6f8d984882a722fef6d2fbf65
Tomar, Akshay <77456961+aktomar@users.noreply.github.com>
2022-10-10 15:15:03 +0530

    Update Testcase_extract_ReleaseDate_and_Country.java

--- src/test/java/com/codingAssignement/TestCases/Testcase_extract_ReleaseDate_and_Country.java
+++ src/test/java/com/codingAssignement/TestCases/Testcase_extract_ReleaseDate_and_Country.java
@@ -10,6 +10,11 @@ public class Testcase_extract_ReleaseDate_and_Country extends BaseClass{
 	public void Extract_ReleaseDate_and_Country() {
 	
 		ExtractDate_and_Country dtCon = new ExtractDate_and_Country(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		WebElement ele = driver.findElement(By.xpath("//*[text()='December 17, 2021 (United States)']"));
+		js.executeScript("arguments[0].scrollIntoView();", ele);
+		Thread.sleep(5000);
+		
 		
 		dtCon.ExtractReleaseDate();
 		
